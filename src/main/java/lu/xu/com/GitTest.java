ee77edc26db2ebe1f60be56afe4914fc98baf8b9
徐陆 <751726462@qq.com>
2022-12-18 20:13:44 +0800

    第9次提交

--- src/main/java/lu/xu/com/GitTest.java
+++ src/main/java/lu/xu/com/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
         System.out.println("hello git33");
         System.out.println("hello git44");
         System.out.println("hello git55，这是我修改的，但是没提交，然后创建分支，这个应该会带入新的分支中去");
+        System.out.println("hello git66");
     }
 
 }
