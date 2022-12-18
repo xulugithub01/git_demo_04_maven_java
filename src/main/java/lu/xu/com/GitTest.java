e3a2a0ccf447b13a6ab7650270af4ad52fa65b64
徐陆 <751726462@qq.com>
2022-12-18 17:48:56 +0800

    第二次提交

--- src/main/java/lu/xu/com/GitTest.java
+++ src/main/java/lu/xu/com/GitTest.java
@@ -3,6 +3,7 @@ package lu.xu.com;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git22");
     }
 
 }
