3242f403e26e57b74a5c9cfcda932f7e8c1645a3
xulugithub01 <46015637+xulugithub01@users.noreply.github.com>
2022-12-20 14:22:31 +0800

    Update GitTest.java

--- src/main/java/lu/xu/com/GitTest.java
+++ src/main/java/lu/xu/com/GitTest.java
@@ -3,7 +3,7 @@ package lu.xu.com;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
-        System.out.println("hello git22");
+        System.out.println("hello git22__测试GitHub和idea本地修改同一行代码的冲突解决");
         System.out.println("hello git33");
         System.out.println("hello git44");
         System.out.println("hello git55，这是我修改的，但是没提交，然后创建分支，这个应该会带入新的分支中去");
