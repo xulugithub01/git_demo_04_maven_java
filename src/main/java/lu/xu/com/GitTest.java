da14c412a6999183d77845bc1d4d34262519a776
xulugithub01 <46015637+xulugithub01@users.noreply.github.com>
2022-12-20 14:18:03 +0800

    Update GitTest.java

--- src/main/java/lu/xu/com/GitTest.java
+++ src/main/java/lu/xu/com/GitTest.java
@@ -8,7 +8,7 @@ public class GitTest {
         System.out.println("hello git44");
         System.out.println("hello git55，这是我修改的，但是没提交，然后创建分支，这个应该会带入新的分支中去");
         System.out.println("hello git66");
-        System.out.println("hello git77");
+        System.out.println("hello git77____我在GitHub上修改一下");
         System.out.println("hello git88，github");
         System.out.println("hello git99，直接在GitHub上修改代码，然后使用idea去拉取 pull 代码 ");
     }
